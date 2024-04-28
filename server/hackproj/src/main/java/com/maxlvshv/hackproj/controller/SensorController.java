8bf9e1085b0f9f6317304b6b2968049da7f50200
Max <clashshow2000@gmail.com>
2024-04-28 14:17:02 +0500

    small changes

--- server/hackproj/src/main/java/com/maxlvshv/hackproj/controller/SensorController.java
+++ server/hackproj/src/main/java/com/maxlvshv/hackproj/controller/SensorController.java
@@ -3,14 +3,12 @@ package com.maxlvshv.hackproj.controller;
 import com.maxlvshv.hackproj.entity.SensorEntity;
 import com.maxlvshv.hackproj.service.SensorService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 @AllArgsConstructor
 public class SensorController {
     private final SensorService sensorService;
