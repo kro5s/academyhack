33f6e7cb1d8930cfabb1be6eb0f19fece2d5a788
Max <clashshow2000@gmail.com>
2024-04-28 14:59:58 +0500

    rebuild 2

--- server/hackproj/src/main/java/com/maxlvshv/hackproj/config/WebConfig.java
+++ server/hackproj/src/main/java/com/maxlvshv/hackproj/config/WebConfig.java
@@ -8,6 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
     }
 }
